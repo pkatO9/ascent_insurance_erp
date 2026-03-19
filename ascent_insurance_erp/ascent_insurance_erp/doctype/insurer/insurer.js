7ef8ac80712b11c3d213bfd29bcc74a88a7ed2c2
Pushpank <pkats.box@gmail.com>
2026-03-19 09:06:16 +0530

    adding insurer doc

---
+++ ascent_insurance_erp/ascent_insurance_erp/doctype/insurer/insurer.js
@@ -0,0 +1,5 @@
+frappe.ui.form.on("Insurer", {
+	refresh(frm) {
+
+	},
+});
---
+++ ascent_insurance_erp/ascent_insurance_erp/doctype/insurer/insurer.py
@@ -0,0 +1,6 @@
+import frappe
+from frappe.model.document import Document
+
+class Insurer(Document):
+	def validate(self):
+		pass
